c1951adcbba88ceaf582522ae6c4e16b4aa08880
EchoSave <echo.salonga@edu.sait.ca>
2026-07-13 20:34:37 -0600

    Update Parser.java

--- assignment2StartingCode/assignment2StartingCode/src/appDomain/Parser.java
+++ assignment2StartingCode/assignment2StartingCode/src/appDomain/Parser.java
@@ -1,10 +1,23 @@
 package appDomain;
 
+import java.io.*;
+import java.util.*;
+import java.util.regex.*;
+
 public class Parser {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	private static final Pattern TAG_PATTERN =
+	        Pattern.compile("<(/?)([^>/\\s]+)[^>]*(/?)>");
+	
+	public static void main(String[] args) throws Exception{
+		
+		if(args.length == 0 ) {
+			System.out.println("For parsing XML files");
+			return;
+		}
+		
+		File file = new File (args[0]);
+		BufferedReader reader = new BufferedReader (new FileReader(file));
 	}
 
 }
