43954bbdd197f136027a904512b25724a9931120
EchoSave <echo.salonga@edu.sait.ca>
2026-07-14 00:24:20 -0600

    XML Parser. Please Look At it

--- assignment2StartingCode/assignment2StartingCode/src/appDomain/Parser.java
+++ assignment2StartingCode/assignment2StartingCode/src/appDomain/Parser.java
@@ -18,6 +18,67 @@ public class Parser {
 		
 		File file = new File (args[0]);
 		BufferedReader reader = new BufferedReader (new FileReader(file));
-	}
+		
+		Stack <String> stack = new Stack<>();
+		
+		boolean rootFound = false;
+		String line;
+		int lineNum = 0 ;
+		
+		while ((line = reader.readLine()) != null) {
+			lineNum++;
+			
+			if(line.trim().startsWith("<?xml")) continue;
+			
+			Matcher match = TAG_PATTERN.matcher(line);
+			
+			while (match.find()) {
+                String isClosing = match.group(1);
+                String tagName = match.group(2);
+                String isSelfClosing = match.group(3);
+                
+                if (isSelfClosing.equals("/")) {
+                	continue;
+                }
+                
+                if (isClosing.equals("/")) {
+                	if(stack.isEmpty()) {
+                		System.out.println("Line " + lineNum +
+                                ": Closing tag </" + tagName +
+                                "> has no matching opening tag.");
+                            continue;
+                	}
+                	
+                	 String openTag = stack.pop();
+                     if (!openTag.equals(tagName)) {
+                         System.out.println("Line " + lineNum +
+                             ": Tag mismatch. Expected </" +
+                             openTag + "> but found </" +
+                             tagName + ">.");
+                     }
+                     continue;
+                }
+                
+                if (!rootFound) {
+                    rootFound = true;
+                } else if (stack.isEmpty()) {
+                    System.out.println("Line " + lineNum +
+                        ": Multiple root elements detected.");
+                }
+                
+                stack.push(tagName);
+			}
+			
+            if (line.contains("<") && !match.find()) {
+                System.out.println("Line " + lineNum +
+                    ": Malformed tag syntax.");
+            }
+        }
 
+        reader.close();
+
+        while (!stack.isEmpty()) {
+            System.out.println("Unclosed tag at end: <" + stack.pop() + ">");
+        }
+    }
 }
